a80a0da2f767a29bb3622339099a35eb28ee8a7b
Justin Lin <ro0566320@gmail.com>
2020-03-22 22:36:42 -0400

    create sample table

--- src/components/Employee-directory.js
+++ src/components/Employee-directory.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
-import NavTabs from "./JumbTab";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Blog from "./pages/Blog";
-import Contact from "./pages/Contact";
-import JumbTab from "./JumbTab";
+import JumboSec from "./JumboSec"
+import TableSec from "./TableSec"
 
-class Portfolio extends Component {
+class EmployeeDirect extends Component {
   state = {
     currentPage: "Home"
   };
@@ -15,26 +11,18 @@ class Portfolio extends Component {
     this.setState({ currentPage: page });
   };
 
-  renderPage = () => {
-    if (this.state.currentPage === "Home") {
-      return <Home />;
-    } else if (this.state.currentPage === "About") {
-      return <About />;
-    } else if (this.state.currentPage === "Blog") {
-      return <Blog />;
-    } else {
-      return <Contact />;
-    }
-  };
+
+
 
   render() {
     return (
       <div>
-        <div className="jumbotron text-center bg-primary"><h1>Employee Directory</h1></div>
-        <p>hello</p>
+        <JumboSec />
+        <br></br><br></br>
+        <TableSec />
       </div>
     );
   }
 }
 
-export default Portfolio;
+export default EmployeeDirect;
---
+++ src/components/JumboSec.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Whenever we try to render an array containing JSX, React knows to render each JSX element separately
+function JumboSec() {
+  return (
+    <div className="jumbotron text-center bg-primary"><h1>Employee Directory</h1></div>
+  );
+}
+
+export default JumboSec;
